7e3b09a1326ef9f8e8389aadbe591521400c08ce
JulaB <julia.bazhuhina@gmail.com>
2019-04-12 15:40:33 -0700

    Fix postcss custom media

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,18 @@ module.exports = {
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-nested'),
-    require('postcss-custom-media'),
+    require('postcss-custom-media')({
+      importFrom: [
+        {
+          customMedia: {
+            '--viewport-sm': '(min-width: 576px)',
+            '--viewport-md': '(min-width: 768px)',
+            '--viewport-lg': '(min-width: 992px)',
+            '--viewport-xl': '(min-width: 1200px)'
+          }
+        },
+      ]
+    }),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
